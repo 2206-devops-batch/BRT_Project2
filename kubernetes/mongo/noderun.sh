d5766b903d0bfc6fc9a2cdaaf5582977ca9be92f
Bryon Smith <webmastersmith37@gmail.com>
2022-07-17 08:59:41 -0500

    minor commit

--- kubernetes/mongo/noderun.sh
+++ kubernetes/mongo/noderun.sh
@@ -1,11 +1,11 @@
-minikube start --driver=docker
+minikube start
 minikube status
 minikube ip
-kubectl get pods all
 kubectl cluster-info
 
 # run yaml
-kubectl apply -f "/home/webmaster/documents/BRT_Project2/kubernetes/mongo-secrets.yaml" 
-kubectl apply -f "/home/webmaster/documents/BRT_Project2/kubernetes/mongo-express-configmap.yaml" 
-kubectl apply -f "/home/webmaster/documents/BRT_Project2/kubernetes/mongodb-express-deployment.yaml" 
-kubectl apply -f "/home/webmaster/documents/BRT_Project2/kubernetes/mongodb-deployment.yaml" 
+kubectl apply -f "/home/webmaster/documents/BRT_Project2/kubernetes/mongo/mongo-secrets.yaml" 
+kubectl apply -f "/home/webmaster/documents/BRT_Project2/kubernetes/mongo/mongo-express-configmap.yaml" 
+kubectl apply -f "/home/webmaster/documents/BRT_Project2/kubernetes/mongo/mongodb-express-deployment.yaml" 
+kubectl apply -f "/home/webmaster/documents/BRT_Project2/kubernetes/mongo/mongodb-deployment.yaml" 
+kubectl get all
