9a2f22f4df27ec54175265eb0ec7972d224f75aa
Bryon Smith <webmastersmith37@gmail.com>
2022-07-18 11:25:57 -0500

    minor commit

--- dockerpush.sh
+++ dockerpush.sh
@@ -6,7 +6,7 @@ python3 -m pytest test-app.py
 
 # This only runs if pytest passes!
 if [[ $? -eq 0 ]]; then
-
+echo "$(git log -1 --pretty=format:"%H")"
 docker build -t "bryonsmith/flaskapp-demo:$(git log -1 --pretty=format:"%H")" . < Dockerfile
 echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
 docker push bryonsmith/flaskapp-demo:v1
