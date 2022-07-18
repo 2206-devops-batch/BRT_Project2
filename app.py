8182f6e0c4bab3fb4d7a1530e9e9f82ea15c4186
Bryon Smith <webmastersmith37@gmail.com>
2022-07-18 16:14:15 -0500

    minor commit

--- app.py
+++ app.py
@@ -2,10 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# home route
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
-#added a typo to test a git push
+
+# added a typo to test a git push
 @app.route("/<name>")
 def hello_name(name):
     return "<p>Hello, " + name + "!</p>"
+
+# add new route goodbye
+@app.route("/goodbye/<name>")
+def hello_name(name):
+    return "<p>Goodbye, " + name + "!</p>"
