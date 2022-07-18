908dc8cd87cf604ac81ee33aac4ea5b1a5cd582f
Bryon Smith <webmastersmith37@gmail.com>
2022-07-18 11:06:29 -0500

    minor commit

--- dockerpush.sh
+++ dockerpush.sh
@@ -1,12 +1,16 @@
 whoami
 pwd
+
 # run python test
 python3 -m pytest test-app.py
+
 # This only runs if pytest passes!
 if [[ $? -eq 0 ]]; then
-docker build -t bryonsmith/flaskapp-demo:v1 . < Dockerfile
+
+docker build -t bryonsmith/flaskapp-demo:$(git log -1 --pretty=format:"%H") . < Dockerfile
 echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
 docker push bryonsmith/flaskapp-demo:v1
+
 else
 echo "Pytest Failure! Docker did not run or push!!!!!!!!!!!!!!!"
 fi
